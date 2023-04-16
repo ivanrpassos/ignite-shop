54e8b34dcbf7d6b524a005492a1f234c97b50804
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-16 09:41:29 -0300

    Aula 14 - Formatando preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     };
   });
 
@@ -77,6 +80,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2,
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
