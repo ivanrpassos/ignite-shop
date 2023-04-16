c5e6fc539b6fd1acc2dd1297372c81e4e2dc80b6
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-16 20:43:54 -0300

    Aula 16 - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,34 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>product: {JSON.stringify(query.id)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        {/* <Image src="" /> */}
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 70,90</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Excepturi fuga repellat
+          doloremque deserunt quia nihil perspiciatis
+          mollitia ut suscipit tempore neque quae enim porro
+          facilis quibusdam officiis incidunt, quaerat est!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
