5369141f53512bcfebb59f66a93a66e121533493
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-18 13:20:13 -0300

    Aula 20 - Prefetch de links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,6 +39,7 @@ export default function Home({ products }: HomeProps) {
           <Link
             href={`/product/${product.id}`}
             key={product.id}
+            prefetch={false}
           >
             <Product className='keen-slider__slide'>
               <Image
